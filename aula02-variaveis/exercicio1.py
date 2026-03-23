da552f2969f484c334ec14f285817a9c64553182
jairferreira8 <fjair08062008@gmail.com>
2026-03-23 19:54:42 -0300

    exercicios 1-5

---
+++ aula02-variaveis/exercicio1.py
@@ -0,0 +1,10 @@
+print("Calcule a area de um circulo:")
+print("Valor aproximado de pi: 3.14159")
+
+
+num1 = float(input("Digite o raio do circulo: "))
+pi = 3.14159
+
+resultado = pi * num1 ** 2
+
+print("Resultado: ", resultado)
---
+++ aula02-variaveis/exercicios5.py
@@ -0,0 +1,8 @@
+print ("Calcule sua média!")
+
+a = input(float("Insira sua primeira nota:"))
+b = input(float("Insira sua segunda nota:"))
+
+media =  (a + b) / 2
+
+print("Sua média é igual a: ", media)
